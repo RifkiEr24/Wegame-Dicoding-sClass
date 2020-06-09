37208aae985aa434db2155215f3aec0e76100659
RifkiEr24 <rifkierlangga17@gmail.com>
2020-06-09 16:43:39 +0700

    finish, js search and language change add + fix nav responsive"

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
 function myFunction() {
-    // Declare variables
+  
     var input, filter,gamelist, game, a, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     gamelist = document.getElementById("gamelist");
     game = document.querySelectorAll(".box");
   
-    // Loop through all list items, and hide those who don't match the search query
+  
     for (i = 0; i < game.length; i++) {
       a = game[i].querySelector(".gamelabel");
       txtValue = a.textContent || a.innerText;
@@ -17,3 +17,41 @@ function myFunction() {
       }
     }
   }
+  // LANGUAGE
+function languagechange(){
+  languagecheck=document.getElementById('languagechange');
+  let account=document.getElementById('account');
+  let gamesplayed=document.getElementById('gamesplayed');
+  let editprofile=document.getElementById('editprofile');
+  let jumbo=document.getElementById('jumbotron');
+  let join=document.querySelectorAll(".join");
+  let footer=document.getElementById('footer');
+  let search=document.getElementById('myInput');
+  let profilenav=document.getElementById('profilenav');
+  let aboutnav=document.getElementById('aboutnav');
+  if(languagecheck.checked===true){
+    account.innerText="Akun";
+    gamesplayed.innerText="Games yang dimainkan";
+    editprofile.innerText="Sunting Profil";
+    jumbo.innerText="KOMUNITAS GAMING TERBAIK DI PENJURU DUNIA";
+    for(i=0;i<join.length;i++){
+      join[i].innerText="Gabung";
+    }
+    footer.innerHTML="Website ini dibuat untuk memenuhi  Tugas Akhir/ Submission dari  Akademi Dicoding Kelas Belajar Dasar Pemrograman Web "
+    search.placeholder="Cari Game disini..";
+    profilenav.innerHTML="Profil";
+    aboutnav.innerHTML="Tentang";
+  }else{
+    account.innerText="Account";
+    gamesplayed.innerText="Games Played";
+    editprofile="Edit Profile";
+    jumbo.innerText="BEST GAMING COMMUNITY AROUND THE WORLD";
+    for(i=0;i<join.length;i++){
+      join[i].innerText="Join";
+    }
+    footer.innerHTML="This Website was created to fulfill the final project / submission from Dicoding's Academy at Basic Web programming class";
+    search.placeholder="Search Game here..";
+    profilenav.innerHTML="Profile";
+    aboutnav.innerHTML="About"
+  }
+}
